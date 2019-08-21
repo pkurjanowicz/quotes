d7f09675bdc1bc5b2935898e5c8493404e10a558
Peter <pkurjanowicz5@gmail.com>
2019-08-20 19:57:07 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById('login-form')
+
+loginForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    const authorSearch = loginForm.querySelector('input[type="text"]').value;
+    const quote = document.getElementById('random-quote');
+    const author = document.getElementById('author');
+    fetch(`https://api.quotable.io/quotes?author=${authorSearch}`)
+        .then(response => response.json())
+        .then(data => {
+            quote.innerHTML = data.results[0].content;
+            author.innerHTML = data.results[0].author;
+        })
+        .catch(error => { quote.innerHTML = "Can't fine a quote :("; } );
+    
+});
